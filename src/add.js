a80aabb2f3c49cf00a1786663ba437a759ae81b5
“Samy <samy.mouahbi@hotmail.com>
2024-10-04 19:36:42 +0200

    solved

--- src/add.js
+++ src/add.js
@@ -1,7 +1,6 @@
-function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+function add(numOne, numTwo) {
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
         return undefined;
     }
-
     return numOne + numTwo;
 }
